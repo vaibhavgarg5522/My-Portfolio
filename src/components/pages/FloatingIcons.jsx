109fc957501070e38575c845c7552d2f6bbc9031
vaibhavgarg5522 <vaibhavgarg5522@gmail.com>
2025-05-02 22:22:35 +0530

    addfile

--- src/components/pages/FloatingIcons.jsx
+++ src/components/pages/FloatingIcons.jsx
@@ -34,14 +34,14 @@ const FloatingIcons = () => {
   ], []); // empty dependency array since values are static
 
   return (
-    <div className="fixed top-[408px] md:top-[428px] right-[10px] flex flex-col gap-4 z-50">
+    <div className="fixed top-[408px] md:top-[328px] right-[18px] flex flex-col gap-4 z-50">
       {icons.map((item, index) => (
         <a
           key={index}
           href={item.link}
           target="_blank"
           rel="noopener noreferrer"
-          className={`w-12 h-12 rounded-full text-white flex items-center justify-center shadow-md hover:scale-110 transition-transform duration-300 ${item.bg}`}
+          className={`w-12 h-12  rounded-full text-white flex items-center justify-center shadow-md hover:scale-110 transition-transform duration-300 ${item.bg}`}
         >
           <span className="text-[22px] icon-wiggle">{item.icon}</span>
         </a>
